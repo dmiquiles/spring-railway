68d6b4e3daa361e950202aca55b4be5169a9320f
Daniel Miquiles <danielvictormiquiles@gmail.com>
2023-08-28 11:41:25 -0300

    feat: criando o projeto

---
+++ spring-railway/src/main/java/miquiles/daniel/springrailway/SpringRailwayApplication.java
@@ -0,0 +1,13 @@
+package miquiles.daniel.springrailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRailwayApplication.class, args);
+	}
+
+}
---
+++ spring-railway/src/test/java/miquiles/daniel/springrailway/SpringRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package miquiles.daniel.springrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
